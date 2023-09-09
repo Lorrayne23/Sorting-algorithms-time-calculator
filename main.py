34e34b3b8805e382a9857ec3b6c0f5607ab9a581
Lorrayne Reis <loohsilva995@gmail.com>
2023-09-08 21:13:52 -0300

    "add main.py"

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+# Criar 50 vetores que devem ser aleatoriamente preenchidos com cada tamanho do vetor está na lista
+
+vectorSizes = [62500, 125000, 250000, 375000]
+
+for size in vectorSizes:
+
+    for _ in range(50):
+        print(str(size))
+        aleatoryVector = [random.randint(1, 100) for _ in range(62500)]
+        #print(aleatoryVector)
+        print("----------------------------------------")
+
