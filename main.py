061740b4cde687fcb41ce435a0461215dd48ecce
Lorrayne Reis <loohsilva995@gmail.com>
2023-09-24 11:49:58 -0300

    add dict of values and time  of merge sort

--- main.py
+++ main.py
@@ -6,32 +6,48 @@ import time
 class AlgorithmsExecution:
     def __init__(self):
         self.vectorSizes = [62500, 125000, 250000, 375000]
-        self.timeExecutionMergeSort = []
+        self.timeExecution = []
+        self.averageTimes = []
 
     def runDefinedValues(self):
         for size in self.vectorSizes:
-            for _ in range(50):
-                print(str(size))
+            print(str(size))
+            for _ in range(10):
                 aleatoryVector = [random.randint(1, 100) for _ in range(size)]
                 self.mergeSortExecution(aleatoryVector)
-                #selection_order = Selection().selectionSort(aleatoryVector)
-                #print("Selection Sort Order:", selection_order)
+            self.averageMergeSortExecution()
+        print(self.averageTimes)
 
     def mergeSortExecution(self,vector):
         start_time = time.time()
         merge_sort = MergeSort().sort(vector)
         end_time = time.time()
         execution_time = end_time - start_time
-        self.timeExecutionMergeSort.append(execution_time)
+        self.timeExecution.append(execution_time)
+
 
     def averageMergeSortExecution(self):
-        average = sum(self.timeExecutionMergeSort) / len(self.timeExecutionMergeSort)
+        print(self.timeExecution)
+        average = sum(self.timeExecution) / len(self.timeExecution)
+        self.averageTimes.append(average)
         print("Average execution time Merge Sort algorithm:", average)
+        self.timeExecution = []
+
+    def defineDictVectorsTime(self):
+        size_time = {}
+        for i in range(len(self.vectorSizes)):
+            size = self.vectorSizes[i]
+            time = self.averageTimes[i]
+            size_time[size] = time  #  o tamanho à média de tempo no dicionário
+
+        # Exibindo o dicionário resultante
+        print(size_time)
 
 
 if __name__ == "__main__":
     benchmark = AlgorithmsExecution()
     benchmark.runDefinedValues()
-    benchmark.averageMergeSortExecution()
+    benchmark.defineDictVectorsTime()
+    #benchmark.averageMergeSortExecution()
 
 
